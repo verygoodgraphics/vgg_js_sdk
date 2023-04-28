9d697c884f3328c0640b6e242d2b9da9dde81f5a
Guanhua Hou <gaimanjing@gmail.com>
2023-04-28 17:19:16 +0800

    updated: getDesignDocument return deep proxy object

--- __tests__/design_document.test.ts
+++ __tests__/design_document.test.ts
@@ -1,12 +1,12 @@
-import { describe, expect, jest, test } from '@jest/globals';
+import { beforeEach, afterAll, describe, expect, jest, test } from '@jest/globals';
 
 import * as DesignDocument from '../src/design_document'
 import * as VggSdk from '../src/basic_sdk'
 
-describe('smoke', () => {
-  test('call getDesignDocument', async () => {
-    // Setup
-    const mockFn = jest.spyOn(VggSdk, 'getVggSdk');
+describe('basic', () => {
+  const mockFn = jest.spyOn(VggSdk, 'getVggSdk');
+  // Setup
+  beforeEach(() => {
     const mockDocument = {
       level: 0,
       a: {
@@ -30,7 +30,13 @@ describe('smoke', () => {
       };
       return Promise.resolve(fakeSdk);
     });
+  });
+  // Teardown
+  afterAll(() => {
+    mockFn.mockRestore();
+  });
 
+  test('design document is object', async () => {
     // Given
     const sut = DesignDocument;
 
@@ -39,8 +45,16 @@ describe('smoke', () => {
 
     // Then
     expect(doc).toBeInstanceOf(Object);
+  })
 
-    // Teardown
-    mockFn.mockRestore();
+  test('design document is proxy object', async () => {
+    // Given
+    const sut = DesignDocument;
+
+    // When
+    const doc = await sut.getDesignDocument();
+
+    // Then
+    expect(sut.isProxy(doc)).toBeTruthy;
   })
 });
--- src/design_document.ts
+++ src/design_document.ts
@@ -1,15 +1,126 @@
 import { getVggSdk } from './basic_sdk';
 
+const _isProxyKey = Symbol();
+const _proxyKey = Symbol();
 
-async function getDesignDocument() {
+interface VggDesignDocumentType {
+  // https://github.com/Microsoft/TypeScript/issues/24587
+  [index: string]: any,
+  // @ts-ignore
+  [index: symbol]: any,
+  _isProxyKey: boolean | null,
+  _proxyKey: VggDesignDocumentType | null
+}
+
+async function getDesignDocument(): Promise<VggDesignDocumentType> {
   const sdk = await getVggSdk();
   const docString = sdk.getDesignDocument();
-  const doc = JSON.parse(docString);
+  const jsonDoc = JSON.parse(docString);
 
-  console.log(doc);
+  const designDoc = makeDeepProxy(jsonDoc);
+  return designDoc;
+}
 
-  return doc;
+function isProxy(doc: VggDesignDocumentType): boolean {
+  return doc[_isProxyKey] ?? false;
 }
 
 
-export { getDesignDocument };
+// internal
+class ProxyHandler {
+  designDoc: VggDesignDocumentType;
+
+  constructor(designDoc: VggDesignDocumentType) {
+    this.designDoc = designDoc;
+  }
+
+  set(obj: VggDesignDocumentType, prop: string, value: any) {
+    let path = getPathToNode(obj[_proxyKey], this.designDoc);
+    console.log(`proxy setter called: obj = ${obj}, prop = ${prop}, value = ${value}, path = ${path}`);
+
+    let proxyObj = makeDeepProxy(value);
+    obj[prop] = proxyObj;
+    return true;
+  }
+  deleteProperty(target: VggDesignDocumentType, prop: string) {
+    if (prop in target) {
+      let path = getPathToNode(target[_proxyKey], this.designDoc);
+      console.log(`proxy delete called: prop = ${prop}, path = ${path + prop}`);
+
+      delete target[prop]._parent;
+      return delete target[prop];
+    }
+    return false;
+  }
+  defineProperty(target: VggDesignDocumentType, prop: string, descriptor: object) {
+    let targetProxy = target[_proxyKey]
+    if (targetProxy) {
+      let path = getPathToNode(targetProxy, this.designDoc);
+      console.log('#handler, defineProperty: ', path);
+      console.log(`define property called: target = ${target}, path = ${path + prop}`);
+    }
+    // todo? make the property deep proxy
+    return Reflect.defineProperty(target, prop, descriptor);
+  }
+}
+
+function getPathToNode(child: VggDesignDocumentType, node: VggDesignDocumentType): string | null {
+  if (child === node) {
+    return '/';
+  } else {
+    for (const [key, value] of Object.entries(node)) {
+      if (child === node) {
+        return `/${key}`;
+      }
+      const subPath = getPathToNode(child, value);
+      if (subPath) {
+        return `/${key}/${subPath}`;
+      }
+    }
+  }
+
+  return null;
+}
+
+function makeDeepProxy(object: VggDesignDocumentType) {
+  if (!object || typeof object != "object") {
+    return object;
+  }
+
+  if (object[_isProxyKey] == true) {
+    return object;
+  }
+
+  let selfProxy = new Proxy(object, new ProxyHandler(object));
+  Object.defineProperty(selfProxy, _isProxyKey, {
+    value: true,
+    writable: false,
+    enumerable: false,
+    configurable: false,
+  });
+
+  const propNames = Reflect.ownKeys(object);
+  for (const name of propNames) {
+    // @ts-ignore
+    const value = object[name];
+
+    if (value && typeof value === "object") {
+      let child = makeDeepProxy(value);
+      // @ts-ignore
+      object[name] = child;
+    }
+  }
+
+  // must after Reflect.ownKeys
+  Object.defineProperty(object, _proxyKey, {
+    value: selfProxy,
+    enumerable: false,
+    configurable: false,
+  });
+
+  return selfProxy;
+}
+
+// todo, observe native update
+
+export { getDesignDocument, isProxy };
